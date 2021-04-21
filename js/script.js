e88724774949d74788970ead819f9912b1fadc04
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-04-21 13:41:01 +0000

    Added Event Listener to navToggle at script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 /*
-    contains - checks classList for specific class
+    contains - shows/gets all classes
     add - add class
     remove - remove class
     toggle - toggles class
 
 */
+
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+navToggle.addEventListener('click', function() {
+    links.classList.toggle('.show-links');
+});
