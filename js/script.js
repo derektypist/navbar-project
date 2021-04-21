1d7e27e91e77f3c8f52221a6b816a96daeb2b647
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-04-21 13:49:54 +0000

    Made minor correction at script.js

--- js/script.js
+++ js/script.js
@@ -9,5 +9,5 @@
 const navToggle = document.querySelector('.nav-toggle');
 const links = document.querySelector('.links');
 navToggle.addEventListener('click', function() {
-    links.classList.toggle('.show-links');
+    links.classList.toggle('show-links');
 });
